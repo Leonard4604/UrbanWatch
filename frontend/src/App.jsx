40db735bdf617d5e31aacaf5a631da78b27b97e3
Leonardo Carrozzo <leonardomaria.c@gmail.com>
2024-03-27 22:52:02 +0100

    Changed context path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,9 @@ import {
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import { useContext } from 'react';
-import injectContext, { Context } from './hooks/appContext';
+import injectContext, { Context } from './store/appContext';
 import NotFound from './pages/NotFound';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   const { store } = useContext(Context);
@@ -17,9 +18,20 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signin" element={store.token ? <LandingPage /> : <SignIn />} />
-        <Route path="/signup" element={store.token ? <LandingPage /> : <SignUp />} />
+        <Route path="/" element={store.token ? <Dashboard /> : <SignIn />} />
+        <Route path="/signin" element={store.token ? <Dashboard /> : <SignIn />} />
+        <Route path="/signup" element={store.token ? <Dashboard /> : <SignUp />} />
+        {!store.token ?
+          <>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </>
+          :
+          <>
+            <Route path="/signin" element={<Dashboard />} />
+            <Route path="/signup" element={<Dashboard />} />
+          </>
+        }
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -13,7 +13,7 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import BasicModal from '../components/BasicModal';
 import { useContext } from 'react';
-import { Context } from '../hooks/appContext';
+import { Context } from '../store/appContext';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
