94f9d008dae493d63c9753f3d45f03c91451e320
Leonardo Carrozzo <leonardomaria.c@gmail.com>
2024-03-27 22:51:37 +0100

    Changed 404 page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,38 +1,56 @@
 import React from 'react';
-import { Box, Button, Container, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid';
+import { Typography, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-export default function NotFound() {
-  return (
-    <Box
-      sx={{
+
+const NotFound = () => {
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate('/'); // Navigating back to '/' path
+      };
+
+    const rootStyle = {
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        minHeight: '100vh',
-        backgroundColor: 'background.default',
-      }}
-    >
-      <Container maxWidth="md">
-        <Grid container spacing={2}>
-          <Grid xs={6}>
-            <Typography variant="h1">
-              404
-            </Typography>
-            <Typography variant="h6" color="primary">
-              The page you’re looking for doesn’t exist.
-            </Typography>
-            <Button variant="contained" href='/'>Back Home</Button>
-          </Grid>
-          <Grid xs={6}>
-            <img
-              src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
-              alt=""
-              width={500} height={250}
-            />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  );
-}
+        justifyContent: 'center',
+        height: '90vh',
+        textAlign: 'center',
+        color: '#000',
+    };
+
+    const headingStyle = {
+        marginBottom: '24px', // Increased spacing between heading and body text
+        color: '#000', // Setting text color to black
+    };
+
+    const bodyTextStyle = {
+        marginBottom: '24px', // Increased spacing between body text and button
+    };
+
+    const buttonStyle = {
+        marginTop: '24px', // Increased spacing above the button
+    };
+
+    return (
+        <div style={rootStyle}>
+        <Typography variant="h1" style={headingStyle}>
+            404 - Page Not Found
+        </Typography>
+        <Typography variant="body1" style={bodyTextStyle}>
+            Sorry, the page you are looking for does not exist.
+        </Typography>
+        <Button
+            variant="contained"
+            color="primary"
+            style={buttonStyle}
+            onClick={handleGoBack}
+        >
+            Go Back
+        </Button>
+        </div>
+    );
+};
+
+export default NotFound;
