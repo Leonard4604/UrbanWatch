f297f08891e1bf5dfe4b13474e45204624202fce
Leonardo Maria Carrozzo <leonardomaria.c@gmail.com>
2024-10-17 04:24:06 +0200

    Removed notification from follow

--- follow/routes/follow_bp.py
+++ follow/routes/follow_bp.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.FollowController import submit_follow, delete_follow, get_follows_by_email, get_follows_notifications
+from controllers.FollowController import submit_follow, delete_follow, get_follows_by_email
 
 follow_bp = Blueprint('follow', __name__)
 
@@ -7,5 +7,3 @@ follow_bp.route('/submit', methods=['POST'])(submit_follow)
 follow_bp.route('/<int:follow_id>', methods=['DELETE'])(delete_follow)
 follow_bp.route('/get_by_email/<string:email>',
               methods=['GET'])(get_follows_by_email)
-follow_bp.route('/get_notifications/',
-              methods=['GET'])(get_follows_notifications)
