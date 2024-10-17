ec5e4974cd438f4a10c9537c1e30067589b26ba1
Leonardo Maria Carrozzo <leonardomaria.c@gmail.com>
2024-10-17 04:23:21 +0200

    Removed notifications from follow

--- follow/controllers/FollowController.py
+++ follow/controllers/FollowController.py
@@ -49,29 +49,3 @@ def get_follows_by_email(email):
     except Exception as e:
         print("Exception:", e)  # Print the specific exception for debugging
         return jsonify({"message": "No follow found"}), 404
-    
-def get_follows_notifications():
-    try:
-        insert_query = """
-        INSERT INTO notification (email, "firstName", "lastName", title, category, report_id)
-        SELECT f.email, r."firstName", r."lastName", r.title, r.category, f.report_id 
-        FROM follow f 
-        JOIN report r ON r.id = f.report_id
-        WHERE NOT EXISTS (
-            SELECT 1 FROM notification n
-            WHERE n.email = f.email 
-            AND n."firstName" = r."firstName" 
-            AND n."lastName" = r."lastName" 
-            AND n.title = r.title 
-            AND n.category = r.category 
-            AND n.report_id = f.report_id
-        );
-        """
-        
-        result = db.session.execute(insert_query)
-        db.session.commit() 
-
-        return jsonify('Notifications inserted'), 200
-    except Exception as e:
-        print("Exception:", e)  # Print the specific exception for debugging
-        return jsonify({"message": "No follow found"}), 404
